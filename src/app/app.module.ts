3ab61632c7fa2145d7d8f286d3d0264e53a0cb79
Jordan <jordandoan@hotmail.com>
2022-04-25 15:41:59 -0700

    added loading spinner for API fetch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -11,6 +11,7 @@ import { WorkComponent } from './work/work.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ToolbarComponent } from './toolbar/toolbar.component'
 
 @NgModule({
@@ -22,11 +23,12 @@ import { ToolbarComponent } from './toolbar/toolbar.component'
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     MatCardModule,
     MatPaginatorModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,30 +11,37 @@ export class HomeComponent implements OnInit, OnChanges {
   @Input() rows: number;
   @Input() pageEvent: PageEvent;
 
-  data: any
-  works: any
+  data: any;
+  works: any;
+  loading: boolean;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.loading = true;
     const works = localStorage.getItem("works" + this.rows)
     if (works) {
       this.data = JSON.parse(works);
       this.works = this.data.message.items;
+      this.loading = false;
     } else {
       this.http.get('https://api.crossref.org/works?rows=' + this.rows).subscribe(res => {
         this.data = res;
         this.works = this.data.message.items
         localStorage.setItem("works" + this.rows, JSON.stringify(res))
+        this.loading = false;
       })
     }
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['pageEvent']) {
+    this.loading = true;
+    if (this.pageEvent && changes['pageEvent']) {
       this.http.get('https://api.crossref.org/works?rows=' + this.rows + '&offset=' + this.pageEvent.pageIndex).subscribe(res => {
         this.data = res;
         this.works = this.data.message.items
+        this.loading = false;
       })
     }
+
   }
 }
