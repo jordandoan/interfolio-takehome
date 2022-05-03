805c66ca5cd3671f3ff5d2d22dda747044bf9218
Jordan <jordandoan@hotmail.com>
2022-05-02 20:45:17 -0700

    created search bar component

---
+++ src/app/search-input/search-input.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.component.html',
+  styleUrls: ['./search-input.component.css']
+})
+export class SearchInputComponent implements OnInit {
+  value = "Sample";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
